5bb0358f92a67dec4eca8bbdb23bcb3319cd14c4
Bas de Bruin <bruin998@gmail.com>
2020-04-23 21:50:32 +0200

    added clear and docs buttons

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var gridItems = [];
 function init() {
 
     gridElement = document.getElementById('grid');
+    gridElement.innerHTML = "";
     for (let i = 0; i < gridSize*gridSize; i++) {
 
         gridItems[i] = new Button(gridElement, i);
@@ -20,13 +21,23 @@ function init() {
 
 }
 
+// functions triggered by buttons: 
 function setMode(event, mode) {
     globalMode = mode;
 
     // toggle button activeness
-    const nav = document.querySelector('nav').childNodes;
+    const nav = document.querySelector('#switch').childNodes;
     for (let b of nav) {
         if (b.classList) b.classList.remove('active')
     }
     event.currentTarget.className += " active";
+}
+function clearGrid() {
+    // remove all timers
+    var highestTimeoutId = setTimeout(";");
+    for (var i = 0; i < highestTimeoutId; i++) {
+        clearTimeout(i);
+    }
+    // reinit grid
+    init();
 }
